f6e0137a71c4a1b1a43bf460dc061555102109e9
Aleksandr <zaytsev.chel@gmail.com>
2023-02-18 17:33:43 +0500

    add Example

---
+++ Example001.array/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] coords = new int[2];
+System.Console.Write("Введите X: ");
+coords[0] = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("\nВведите Y: ");
+coords[1] = Convert.ToInt32(Console.ReadLine());
+
+if (coords[0] > 0 && coords[1] > 0)
+{
+    System.Console.WriteLine("Мы находимся в первой четверти");
+}
+else if (coords[0] < 0 && coords[1] > 0)
+{
+    System.Console.WriteLine("Мы находимся во второй четверти");
+}
+else if (coords[0] < 0 && coords[1] < 0)
+{
+    System.Console.WriteLine("Мы находимся в третьей четверти");
+}
+else if (coords[0] > 0 && coords[1] < 0)
+{
+    System.Console.WriteLine("Мы находимся в четвертой четверти");
+}
+else
+{
+    System.Console.WriteLine("Координата лежит на осях");
+}
---
+++ Example002.array/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Введите номер четверти ");
+int number = Convert.ToInt32(Console.ReadLine());
+string[] strArray = {"x > 0 && y > 0", "x < 0 && y > 0", "x < 0 && y < 0","x > 0 && y < 0", "Такой четверти нет"};
+
+if(number > 0 && number < 5)
+{
+  System.Console.WriteLine(strArray[number-1]);
+}
+else
+{
+  System.Console.WriteLine("Такой четверти не существует");
+}
