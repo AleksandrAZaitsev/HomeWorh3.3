9ba2ad6264432e3a6989c6041e5a71b6fab8525e
Aleksandr <zaytsev.chel@gmail.com>
2023-02-18 17:21:44 +0500

    add Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,31 @@
+﻿System.Console.Write("Введи х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введи y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    System.Console.WriteLine("Мы находимся в первой четверти");
+}
+
+else if (x < 0 && y > 0)
+{
+    System.Console.WriteLine("Мы находимся во второй четверти");
+}
+
+else if (x < 0 && y < 0)
+{
+    System.Console.WriteLine("Мы находимся в третьей четверти");
+}
+
+else if (x > 0 && y < 0)
+{
+    System.Console.WriteLine("Мы находимся во четвертой четверти");
+}
+
+else
+{
+    System.Console.WriteLine("в центре");
+}
+
